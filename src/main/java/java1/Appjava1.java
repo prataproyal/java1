7dabe3af9b821c429dd7bfa7f809befe1175ea8a
prathap <prataproyal1995@gmail.com>
2018-02-01 09:43:54 +0530

    first commit

---
+++ src/main/java/java1/Appjava1.java
@@ -0,0 +1,6 @@
+package java1;
+
+public class Appjava1 {
+	public String sample() {
+		return "hi";}
+}
---
+++ src/test/java/java1/Apptestjava1.java
@@ -0,0 +1,14 @@
+package java1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptestjava1 {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
